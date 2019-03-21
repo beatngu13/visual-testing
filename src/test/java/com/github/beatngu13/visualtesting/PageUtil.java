55120e84d3f6b42f2ccfa05f918503314c4a8953
Daniel Kraus <daniel.kraus@mailbox.org>
2019-03-21 10:20:56 +0100

    Add `PageUtil`

---
+++ src/test/java/com/github/beatngu13/visualtesting/PageUtil.java
@@ -0,0 +1,31 @@
+package com.github.beatngu13.visualtesting;
+
+import java.net.MalformedURLException;
+import java.nio.file.Paths;
+
+class PageUtil {
+
+	enum Page {
+		WIKIPEDIA
+	}
+
+	private static final String BASE_PATH = "src/test/resources/";
+
+	public static String get(Page sut) {
+		switch (sut) {
+		case WIKIPEDIA:
+			return getUrlString("wikipedia/characterization-test.html");
+		default:
+			throw new IllegalArgumentException("No SUT '" + sut + "' available.");
+		}
+	}
+
+	private static final String getUrlString(final String relativePath) {
+		try {
+			return Paths.get(BASE_PATH, relativePath).toUri().toURL().toString();
+		} catch (final MalformedURLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
