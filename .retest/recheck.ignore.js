82d1ffbd824718ab83fe16fc4d3b780238a7d3f5
Daniel Kraus <daniel.kraus@mailbox.org>
2019-03-21 22:41:14 +0100

    Improve ignores

--- .retest/recheck.ignore.js
+++ .retest/recheck.ignore.js
@@ -1,12 +1,21 @@
 var MAX_PIXEL_DIFF = 10
 
 function shouldIgnoreAttributeDifference(element, diff) {
-	if (diff.key == "outline") {
-		var ignoreXDiff = Math.abs(diff.expected.x - diff.actual.x) <= MAX_PIXEL_DIFF;
-		var ignoreYDiff = Math.abs(diff.expected.y - diff.actual.y) <= MAX_PIXEL_DIFF;
-		var ignoreWidthDiff = Math.abs(diff.expected.width - diff.actual.width) <= MAX_PIXEL_DIFF;
-		var ignoreHeightDiff = Math.abs(diff.expected.height - diff.actual.height) <= MAX_PIXEL_DIFF;
-		return ignoreXDiff && ignoreYDiff && ignoreWidthDiff && ignoreHeightDiff;
+	var expected = diff.expected;
+	if (expected == null) {
+		expected = "";
 	}
+
+	var actual = diff.actual;
+	if (actual == null) {
+		actual = "";
+	}
+
+	if (expected.indexOf("px") !== -1) {
+		expected = expected.replace("px", "");
+		actual = actual.replace("px", "");
+		return Math.abs(expected - actual) <= MAX_PIXEL_DIFF;
+	}
+
 	return false;
 }
