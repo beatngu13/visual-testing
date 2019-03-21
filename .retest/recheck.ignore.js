21ee0c6e3f0c0d2a164c8a58914c6dd2a6c4fef6
Daniel Kraus <daniel.kraus@mailbox.org>
2019-03-21 21:48:39 +0100

    Ignore small pixel diffs and update golden master

--- .retest/recheck.ignore.js
+++ .retest/recheck.ignore.js
@@ -1,9 +1,12 @@
-// Here you can implement ignore rules for recheck in javascript.
-// Please do not delete this file, even if it is empty.
+var MAX_PIXEL_DIFF = 10
 
-// You can implement either of these two functions:
-// shouldIgnoreElement(element)
-// shouldIgnoreAttributeDifference(element, diff)
-
-// You can find more details and example rules at: 
-// https://retest.github.io/docs/recheck/how-ignore-works-in-recheck/
+function shouldIgnoreAttributeDifference(element, diff) {
+	if (diff.key == "outline") {
+		var ignoreXDiff = Math.abs(diff.expected.x - diff.actual.x) <= MAX_PIXEL_DIFF;
+		var ignoreYDiff = Math.abs(diff.expected.y - diff.actual.y) <= MAX_PIXEL_DIFF;
+		var ignoreWidthDiff = Math.abs(diff.expected.width - diff.actual.width) <= MAX_PIXEL_DIFF;
+		var ignoreHeightDiff = Math.abs(diff.expected.height - diff.actual.height) <= MAX_PIXEL_DIFF;
+		return ignoreXDiff && ignoreYDiff && ignoreWidthDiff && ignoreHeightDiff;
+	}
+	return false;
+}
