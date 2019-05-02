792055f80fa633670ee13948ff7a1ec466af808b
Daniel Kraus <daniel.kraus@mailbox.org>
2019-05-02 16:51:55 +0200

    Add new pages

--- src/test/java/com/github/beatngu13/visualtesting/util/PageFactory.java
+++ src/test/java/com/github/beatngu13/visualtesting/util/PageFactory.java
@@ -6,12 +6,24 @@ import java.nio.file.Paths;
 public class PageFactory {
 
 	public enum Page {
+		LOGIN_V1,
+		LOGIN_V2,
+		APP_V1,
+		APP_V2
 	}
 
 	private static final String BASE_PATH = "src/test/resources/";
 
 	public static String get(Page page) {
 		switch (page) {
+		case LOGIN_V1:
+            return getUrlString("pages/acme-login-v1.html");
+		case LOGIN_V2:
+            return getUrlString("pages/acme-login-v2.html");
+		case APP_V1:
+            return getUrlString("pages/acme-app-v1.html");
+		case APP_V2:
+            return getUrlString("pages/acme-app-v2.html");
 		default:
 			throw new IllegalArgumentException("No page '" + page + "' available.");
 		}
