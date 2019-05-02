3711eed4b1fb4a958d984017eb8aca5f64b507a0
Daniel Kraus <daniel.kraus@mailbox.org>
2019-05-02 18:55:35 +0200

    Update ignore files

--- .retest/recheck.ignore.js
+++ .retest/recheck.ignore.js
@@ -1,21 +1,52 @@
-var MAX_PIXEL_DIFF = 10
+var ALLOWED_PIXEL_DIFF = 10;
+var baseUrl = /http[s]?:\/\/[\w.:\d\-]*/;
+var fontFamilies = [ [ "system-ui", "Arial" ], [ "-apple-system", "sans-serif" ] ];
+
+function contains(array, key) {
+	for (var i = 0; i < array.length; i++) {
+		if (key == array[i]) {
+			return true;
+		}
+	}
+	return false;
+}
 
 function shouldIgnoreAttributeDifference(element, diff) {
-	var expected = diff.expected;
-	if (expected == null) {
-		expected = "";
+	if (diff.key == 'outline') {
+		return (Math.abs(diff.expected.x - diff.actual.x) <= ALLOWED_PIXEL_DIFF)
+				&& (Math.abs(diff.expected.y - diff.actual.y) <= ALLOWED_PIXEL_DIFF)
+				&& (Math.abs(diff.expected.width - diff.actual.width) <= ALLOWED_PIXEL_DIFF)
+				&& (Math.abs(diff.expected.height - diff.actual.height) <= ALLOWED_PIXEL_DIFF);
 	}
-
-	var actual = diff.actual;
-	if (actual == null) {
-		actual = "";
+	if (diff.expected === null) {
+		diff.expected = "";
 	}
-
-	if (expected.indexOf("px") !== -1) {
-		expected = expected.replace("px", "");
-		actual = actual.replace("px", "");
-		return Math.abs(expected - actual) <= MAX_PIXEL_DIFF;
+	if (diff.actual === null) {
+		diff.actual = "";
 	}
-
+	if (contains([ 'absolute-x', 'absolute-y', 'absolute-width',
+			'absolute-height', 'x', 'y', 'width', 'height' ], diff.key)) {
+		return (Math.abs(diff.expected.replace("px", "")
+				- diff.actual.replace("px", "")) <= ALLOWED_PIXEL_DIFF);
+	}
+	if (diff.expected.indexOf("px") !== -1) {
+		return (Math.abs(diff.expected.replace("px", "")
+				- diff.actual.replace("px", "")) <= ALLOWED_PIXEL_DIFF);
+	}
+	if (diff.key == "opacity") {
+		return (Math.abs(diff.expected - diff.actual) <= 10);
+	}
+	if (diff.key == "font-family") {
+		for (var i = 0; i < fontFamilies.length; i++) {
+			if (contains(fontFamilies[i], diff.expected)) {
+				return contains(fontFamilies[i], diff.actual);
+			}
+		}
+	}
+	if (diff.expected != null && diff.actual != null) {
+		cleanExpected = diff.expected.replace(baseUrl, '');
+		cleanActual = diff.actual.replace(baseUrl, '');
+		return cleanExpected === cleanActual;
+	}
 	return false;
 }
