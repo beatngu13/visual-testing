48403cb53650620b737be3369a06aab4fd04be9f
Daniel Kraus <daniel.kraus@mailbox.org>
2019-05-09 22:40:55 +0200

    Update ignore files

--- .retest/recheck.ignore.js
+++ .retest/recheck.ignore.js
@@ -1,6 +1,4 @@
-var ALLOWED_PIXEL_DIFF = 10;
-var baseUrl = /http[s]?:\/\/[\w.:\d\-]*/;
-var fontFamilies = [ [ "system-ui", "Arial" ], [ "-apple-system", "sans-serif" ] ];
+var ALLOWED_PIXEL_DIFF = 25;
 
 function contains(array, key) {
 	for (var i = 0; i < array.length; i++) {
@@ -12,41 +10,38 @@ function contains(array, key) {
 }
 
 function shouldIgnoreAttributeDifference(element, diff) {
-	if (diff.key == 'outline') {
-		return (Math.abs(diff.expected.x - diff.actual.x) <= ALLOWED_PIXEL_DIFF)
-				&& (Math.abs(diff.expected.y - diff.actual.y) <= ALLOWED_PIXEL_DIFF)
-				&& (Math.abs(diff.expected.width - diff.actual.width) <= ALLOWED_PIXEL_DIFF)
-				&& (Math.abs(diff.expected.height - diff.actual.height) <= ALLOWED_PIXEL_DIFF);
-	}
 	if (diff.expected === null) {
 		diff.expected = "";
 	}
 	if (diff.actual === null) {
 		diff.actual = "";
 	}
-	if (contains([ 'absolute-x', 'absolute-y', 'absolute-width',
-			'absolute-height', 'x', 'y', 'width', 'height' ], diff.key)) {
-		return (Math.abs(diff.expected.replace("px", "")
-				- diff.actual.replace("px", "")) <= ALLOWED_PIXEL_DIFF);
+	if (ignorePixelDiff(element, diff)) {
+		return true;
 	}
-	if (diff.expected.indexOf("px") !== -1) {
-		return (Math.abs(diff.expected.replace("px", "")
-				- diff.actual.replace("px", "")) <= ALLOWED_PIXEL_DIFF);
-	}
-	if (diff.key == "opacity") {
-		return (Math.abs(diff.expected - diff.actual) <= 10);
-	}
-	if (diff.key == "font-family") {
-		for (var i = 0; i < fontFamilies.length; i++) {
-			if (contains(fontFamilies[i], diff.expected)) {
-				return contains(fontFamilies[i], diff.actual);
-			}
-		}
-	}
-	if (diff.expected != null && diff.actual != null) {
-		cleanExpected = diff.expected.replace(baseUrl, '');
-		cleanActual = diff.actual.replace(baseUrl, '');
-		return cleanExpected === cleanActual;
+	if (ignoreColorDiff(element, diff)) {
+		return true;
 	}
 	return false;
+}
+
+function ignorePixelDiff(element, diff) {
+	if (diff.key === "outline") {
+		return (Math.abs(diff.expected.x - diff.actual.x) <= ALLOWED_PIXEL_DIFF)
+				&& (Math.abs(diff.expected.y - diff.actual.y) <= ALLOWED_PIXEL_DIFF)
+				&& (Math.abs(diff.expected.width - diff.actual.width) <= ALLOWED_PIXEL_DIFF)
+				&& (Math.abs(diff.expected.height - diff.actual.height) <= ALLOWED_PIXEL_DIFF);
+	}
+	if (contains([ "absolute-x", "absolute-y", "absolute-width", "absolute-height",
+				   "x", "y", "width", "height" ],
+				   diff.key)) {
+		return (Math.abs(diff.expected.replace("px", "")
+				- diff.actual.replace("px", "")) <= ALLOWED_PIXEL_DIFF);
+	}
+}
+
+function ignoreColorDiff(element, diff) {
+	if (diff.key.indexOf("border-") !== -1) {
+		return true;
+	}
 }
