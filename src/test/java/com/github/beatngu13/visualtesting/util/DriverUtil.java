c50a699cb99ff4b469c00b38352e95c1696da195
Daniel Kraus <daniel.kraus@mailbox.org>
2019-05-10 19:57:27 +0200

    Add util for browser name

---
+++ src/test/java/com/github/beatngu13/visualtesting/util/DriverUtil.java
@@ -0,0 +1,17 @@
+package com.github.beatngu13.visualtesting.util;
+
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class DriverUtil {
+
+	private DriverUtil() {
+	}
+
+	public static String getName( WebDriver driver ) {
+		Capabilities cap = ((RemoteWebDriver) driver).getCapabilities();
+		return cap.getBrowserName().toLowerCase();
+	}
+
+}
