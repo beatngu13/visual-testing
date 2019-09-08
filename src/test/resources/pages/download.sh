3daa45d955111ab15af2a70b42062f775e9967b3
Daniel Kraus <daniel.kraus@mailbox.org>
2019-09-08 16:44:13 +0200

    Add shebang

--- src/test/resources/pages/download.sh
+++ src/test/resources/pages/download.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 USER_AGENT='Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:68.0) Gecko/20100101 Firefox/68.0'
 
 # See https://en.wikipedia.org/wiki/List_of_most_popular_websites.
