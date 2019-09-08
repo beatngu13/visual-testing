c92f584419ff00b23f006aa5a04db4bcd4425aaa
Daniel Kraus <daniel.kraus@mailbox.org>
2019-09-08 17:31:54 +0200

    Simplify `DriverFactory`

--- src/test/java/com/github/beatngu13/visualtesting/util/DriverFactory.java
+++ src/test/java/com/github/beatngu13/visualtesting/util/DriverFactory.java
@@ -1,6 +1,5 @@
 package com.github.beatngu13.visualtesting.util;
 
-import java.util.Arrays;
 import java.util.stream.Stream;
 
 import org.openqa.selenium.WebDriver;
@@ -15,25 +14,10 @@ public class DriverFactory {
 	private static final String DESKTOP_WINDOW_SIZE = "--window-size=1920,1080";
 	private static final String NOTEBOOK_WINDOW_SIZE = "--window-size=1366,768";
 
-	public enum Driver {
-		CHROME, FIREFOX
-	}
-
-	public static WebDriver get(final Driver driver) {
-		switch (driver) {
-		case CHROME: {
-			return new ChromeDriver(new ChromeOptions().addArguments(DESKTOP_WINDOW_SIZE));
-		}
-		case FIREFOX: {
-			return new FirefoxDriver(new FirefoxOptions().addArguments(NOTEBOOK_WINDOW_SIZE));
-		}
-		default:
-			throw new IllegalArgumentException("No driver '" + driver + "' driver available.");
-		}
-	}
-
 	public static Stream<WebDriver> getAll() {
-		return Arrays.stream(Driver.values()).map(DriverFactory::get);
+		final var chrome = new ChromeDriver(new ChromeOptions().addArguments(DESKTOP_WINDOW_SIZE));
+		final var firefox = new FirefoxDriver(new FirefoxOptions().addArguments(NOTEBOOK_WINDOW_SIZE));
+		return Stream.of(chrome, firefox);
 	}
 
 }
