f552f5eba3cfff21606735f978176b35d6764e6c
Daniel Kraus <daniel.kraus@mailbox.org>
2019-10-09 12:38:43 +0200

    Add offline pages

---
+++ src/test/resources/pages/s.pinimg.com/webapp/js/pjs-vendor-react-24962dd8b720d6bdd3ab.mjs
@@ -0,0 +1,2 @@
+(window.__LOADABLE_LOADED_CHUNKS__=window.__LOADABLE_LOADED_CHUNKS__||[]).push([["vendor-react"],{1:function(n,e){},8:function(n,e,r){r("KBbs"),r("Mr4W"),r("Ivf5"),r("JsRk"),r("GyhR"),r("9Y9K"),n.exports=r("rYws")},rYws:function(n,e,r){r("Ali/"),r("6vCv")}},[[8,"runtime","vendors~entryChunk-amp-extension~entryChunk-amp-iframe~entryChunk-analytics~entryChunk-business~entr~a548fe00","vendors~vendor-react"]]]);
+//# sourceMappingURL=pjs-vendor-react-24962dd8b720d6bdd3ab.mjs.map
---
+++ src/test/resources/pages/www.xinhuanet.com/fireman19-109/fireman109.js
@@ -0,0 +1,6 @@
+$(function () {
+    $('.fireman109-off').on('click', function () {
+        $('.fireman109-bg').removeClass('fireman109-bg');
+        $('.fireman109-title').remove();
+    });
+});
