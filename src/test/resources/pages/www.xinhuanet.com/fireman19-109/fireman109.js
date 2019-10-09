79e3ea63545c99ba3396164dc07bee5467d7d8aa
Daniel Kraus <daniel.kraus@mailbox.org>
2019-10-09 18:40:11 +0200

    Update offline pages

--- src/test/resources/pages/www.xinhuanet.com/fireman19-109/fireman109.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-    $('.fireman109-off').on('click', function () {
-        $('.fireman109-bg').removeClass('fireman109-bg');
-        $('.fireman109-title').remove();
-    });
-});
