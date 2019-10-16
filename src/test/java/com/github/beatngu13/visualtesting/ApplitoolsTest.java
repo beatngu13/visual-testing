a0ad6d8e57e2dffc3b886c0650846c743d7ed10e
Daniel Kraus <daniel.kraus@mailbox.org>
2019-10-16 19:54:01 +0200

    (Temporarily) disable Applitools test
    Currently no license available.

--- src/test/java/com/github/beatngu13/visualtesting/ApplitoolsTest.java
+++ src/test/java/com/github/beatngu13/visualtesting/ApplitoolsTest.java
@@ -2,6 +2,7 @@ package com.github.beatngu13.visualtesting;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -17,6 +18,7 @@ import com.github.beatngu13.visualtesting.util.TimingExtension;
 /**
  * Tests with Applitools.
  */
+@Disabled
 @ExtendWith(TimingExtension.class)
 class ApplitoolsTest {
 
