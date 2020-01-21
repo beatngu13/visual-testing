20f781ad73b6e4d75ec488448ecf73dbeb733f6c
Creepow <pascalxallion@gmail.com>
2020-01-21 14:41:19 +0000

    Add new pages

---
+++ src/test/resources/newpages/s.pinimg.com/webapp/js/pjs-vendor-react-18a8457cf4f2ea590442.mjs
@@ -0,0 +1,2 @@
+(window.__LOADABLE_LOADED_CHUNKS__=window.__LOADABLE_LOADED_CHUNKS__||[]).push([["vendor-react"],{0:function(_,n){},"3RPo":function(_,n,o){o("CX2u"),o("Wr5T")},7:function(_,n,o){o("3RPo"),o("FoZm"),o("i8i4"),o("/MKj"),o("q1tI"),o("fArA"),_.exports=o("dtw8")}},[[7,"runtime",11,87]]]);
+//# sourceMappingURL=pjs-vendor-react-18a8457cf4f2ea590442.mjs.map
